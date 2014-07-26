49ef34fa468d86accfd5d4bf43dc3d09c13f506e
Tijn Schuurmans <tijn@tijnschuurmans.nl>
2014-07-26 22:23:00 +0200

    explain why I hate this code

--- lib/today/calendar.rb
+++ lib/today/calendar.rb
@@ -3,6 +3,15 @@ require 'open-uri'
 
 module Today
 
+  # Why does this class exist?
+  # it fetches the xml from google calendar
+  # and it has two methods that help with finding the info we want from the resulting XML
+  #
+  # But why do I do these silly things? Why does this class have two tasks!? I kind of hate it already.
+  # Man... what was I thinking?
+  # URI is perfect for putting some extra params in the url
+  # and open-uri is perfectly able to fetch something
+  # this class should just be a thin wrapper around a Nokogiri-document to query for the right info.
   class Calendar
     def initialize(url)
       @page = fetch_xml(url)
