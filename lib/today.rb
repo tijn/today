c1b2f284ae103cd05dc93fa287d7b1147543f933
Tijn Schuurmans <tijn@tijnschuurmans.nl>
2014-07-26 23:46:53 +0200

    fix mistakes, make it work

--- lib/today.rb
+++ lib/today.rb
@@ -4,11 +4,12 @@ module Today
   VERSION = "1.0.1"
 
   def self.fetch_calendar(url)
-    Calendar.new(open(calendar_url_for_today))
+    Calendar.new(open(calendar_url_for_today(url)))
   end
 
-  def self.calendar_url_for_today(today = Date.today)
+  def self.calendar_url_for_today(url, today = Date.today)
     uri = URI.parse(url)
+    tomorrow = today + 1
     start_min = "start-min=#{today}"
     start_max = "start-max=#{tomorrow}"
     uri.query = [uri.query, start_min, start_max].compact.join('&')
