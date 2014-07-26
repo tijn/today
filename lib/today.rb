039296948e5d9e4dac00135410e71645e33c4933
Tijn Schuurmans <tijn@tijnschuurmans.nl>
2014-07-26 22:40:51 +0200

    remove calendar fetching methods from class

--- lib/today.rb
+++ lib/today.rb
@@ -1,5 +1,17 @@
-module Today
-  VERSION = "1.0.0"
-end
-
 require 'today/calendar'
+
+module Today
+  VERSION = "1.0.1"
+
+  def self.fetch_calendar(url)
+    Calendar.new(open(calendar_url_for_today))
+  end
+
+  def self.calendar_url_for_today(today = Date.today)
+    uri = URI.parse(url)
+    start_min = "start-min=#{today}"
+    start_max = "start-max=#{tomorrow}"
+    uri.query = [uri.query, start_min, start_max].compact.join('&')
+    uri.to_s
+  end
+end
--- lib/today/calendar.rb
+++ lib/today/calendar.rb
@@ -3,51 +3,24 @@ require 'open-uri'
 
 module Today
 
-  # Why does this class exist?
-  # it fetches the xml from google calendar
-  # and it has two methods that help with finding the info we want from the resulting XML
-  #
-  # But why do I do these silly things? Why does this class have two tasks!? I kind of hate it already.
-  # Man... what was I thinking?
-  # URI is perfect for putting some extra params in the url
-  # and open-uri is perfectly able to fetch something
-  # this class should just be a thin wrapper around a Nokogiri-document to query for the right info.
+  # wrapper around the xml google calendar returns
   class Calendar
-    def initialize(url)
-      @page = fetch_xml(url)
+    attr_reader :doc
+
+    def initialize(xml)
+      @doc = Nokogiri.parse(xml)
     end
 
     def name
-      @page.css('feed > title').text
+      @doc.css('feed > title').text
     end
 
     def events
       events = []
-      @page.search('entry').each do |entry|
+      @doc.search('entry').each do |entry|
         events << entry.css('title').text
       end
       events
-    end
-
-    private
-
-    def url_with_query(base_url)
-      base_url + "?" + url_args.join('&')
-    end
-
-    def fetch_raw(base_url)
-      open url_with_query(base_url)
-    end
-
-    def fetch_xml(base_url)
-      Nokogiri.parse fetch_raw(base_url)
-    end
-
-    def url_args(today = Date.today)
-      tomorrow = today + 1
-      start_min = "start-min=#{today}"
-      start_max = "start-max=#{tomorrow}"
-      [start_min, start_max]
     end
   end
 
